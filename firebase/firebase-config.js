f07346aee9aa45d7fa25b96711749e6853591c57
ragulkumar1116-source <ragulkumar1116@gmail.com>
2026-06-08 19:39:04 +0530

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -11,13 +11,15 @@
  */
 
 const FIREBASE_CONFIG = {
-     apiKey: "AIzaSyBAaXVxwpkLx1bKXaBp1vbAGQBLf19rRck",
+
+  apiKey: "AIzaSyBAaXVxwpkLx1bKXaBp1vbAGQBLf19rRck",
   authDomain: "church-youth-manager-8a895.firebaseapp.com",
   databaseURL: "https://church-youth-manager-8a895-default-rtdb.firebaseio.com",
   projectId: "church-youth-manager-8a895",
   storageBucket: "church-youth-manager-8a895.firebasestorage.app",
   messagingSenderId: "829854911697",
   appId: "1:829854911697:web:3df4bf5ea2e4b71eb409f0",
+
 };
 
 // Firebase Database Reference Keys
